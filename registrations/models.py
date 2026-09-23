ba2091041a82781a9c6d00988626223316494885
kainatabdulrasheed <259586191+kainatabdulrasheed@users.noreply.github.com>
2026-09-23 12:42:29 +0500

    Add Event and Registration models with migrations

--- registrations/models.py
+++ registrations/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from events.models import Event
 
-# Create your models here.
+class Registration(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    registered_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        unique_together = ('user', 'event')
