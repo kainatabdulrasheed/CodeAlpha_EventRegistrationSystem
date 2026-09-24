0c09ac2a49b72404e1c5a5d87f02efda86834acc
kainatabdulrasheed <259586191+kainatabdulrasheed@users.noreply.github.com>
2026-09-24 15:40:43 +0500

    Add API views for events and registrations

--- events/views.py
+++ events/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Event
+from .serializers import EventSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+@api_view(['GET'])
+def event_list(request):
+    events = Event.objects.all()
+    serializer = EventSerializer(events, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def event_detail(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    serializer = EventSerializer(event)
+    return Response(serializer.data)
+
--- registrations/views.py
+++ registrations/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Registration
+from .serializers import RegistrationSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+@api_view(['POST'])
+def create_registration(request):
+    serializer = RegistrationSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(user=request.user)
+        return Response(serializer.data)
+    return Response(serializer.errors, status=400)
+
+@api_view(['GET'])
+def my_registrations(request):
+    registrations = Registration.objects.filter(user=request.user)
+    serializer = RegistrationSerializer(registrations, many=True)
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def cancel_registration(request, pk):
+    registration = get_object_or_404(Registration, pk=pk, user=request.user)
+    registration.delete()
+    return Response(status=204)
