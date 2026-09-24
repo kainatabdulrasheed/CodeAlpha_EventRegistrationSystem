b81dc76569e5cc26cb418d111ecf31be75dc587c
kainatabdulrasheed <259586191+kainatabdulrasheed@users.noreply.github.com>
2026-09-24 18:04:38 +0500

    Fix RegistrationSerializer to accept event ID as input and return full event details in response

--- registrations/serializers.py
+++ registrations/serializers.py
@@ -5,8 +5,12 @@ from events.serializers import EventSerializer
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
-    event = EventSerializer(read_only=True)
-
     class Meta:
         model = Registration
         fields = '__all__'
+        read_only_fields = ['user']
+
+    def to_representation(self, instance): 
+        data = super().to_representation(instance)
+        data['event'] = EventSerializer(instance.event).data
+        return data
